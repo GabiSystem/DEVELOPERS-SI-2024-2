144dbc71dbf8866b2a5949ce18f03cc109994e26
GabiSystem <gabisystemdev@gmail.com>
2024-10-24 13:56:32 -0300

    Delete otaviomarquesSITE/api directory

--- otaviomarquesSITE/api/my-api.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    const apiID = process.env.SERVICE_ID;
-    const templateID = process.env.TEMPLATE_ID;
-    const processID = process.env.SERVICE_ID;
-
-    res.status(200).json({ apiID, templateID, processID });
-}
